a94ce9934ff5c574506c54b065bbcf30d21ac814
farzinbrat-blip <farzinbrat@gmail.com>
2026-08-22 10:04:02 +0500

    Add main App component with LiquidLensNav

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { LiquidLensNav } from "./components/LiquidLensNav";
+
+export default function App() {
+  return (
+    <div style={{ minHeight: "100vh", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <LiquidLensNav />
+    </div>
+  );
+}
